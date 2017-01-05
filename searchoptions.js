469d9bec89aaaf66f3e903cc6f09f235333c3c4b
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2017-01-05 11:58:50 +0100

    Update 20170105

--- searchoptions.js
+++ searchoptions.js
@@ -1,69 +1,52 @@
 /****************************************************************/
-
-
 /****************************************************************/
 
 var background = browser.extension.getBackgroundPage();
 
-
-/*****************************************************************
-
-document.getElementById("saveOptions").addEventListener("click", function() {
-	while(background.searchEngines.pop());
-	var sE = document.getElementById("searchEngines");
-	for(var i = 1 ; i < sE.rows.length ; i++)
-	{		
-		background.addSearchEngine( i,
-									sE.rows[i].cells[0].children[0].value,
-									sE.rows[i].cells[1].children[0].value,
-									sE.rows[i].cells[2].children[0].checked,
-									sE.rows[i].cells[3].children[0].checked,
-									(sE.rows[i].cells[4].children[0].value=="S"?0:1));
-		
-	}	
-	background.saveOptions();	
-});	
-
-/**************************************************************************** */
-
 function addSearchEngine(eng)
 {
-	$('#engs-cnt').append("<div class='draggable-item-list' data-uid='" + eng.id +	"'><p class='title'>" + eng.name + "</p>"													
+	var item = $("<div class='draggable-item-list' data-uid='" + eng.id +	"'><p class='title'>" + eng.name + "</p>"							
+				+ "<i class='material-icons ico-white ico btn-delete'>close</i>"
+				+ "<i class='material-icons ico-white ico btn-edit'>mode_edit</i>"
+				
+				+ "<div class='edit-engs'><form>"
+				+ "Name: <input class='eng-in' type='text' value='" + eng.name + "'/><br/>"
+				+ "Url: <input class='eng-in' type='text' value='" + eng.url + "'/>"
+				+ "<br/><input value='save' type='button' /><input value='Default' type='reset'/>"
+				+ "</form>"
+				+ "</div></div>");
+	$('#engs-cnt').append(item);
 							
-							+ "<i class='material-icons ico-white ico btn-delete'>close</i>"
-							+ "<i class='material-icons ico-white ico btn-edit'>mode_edit</i>"
-							
-							+ "<div class='edit-engs'><form>"
-							+ "Name: <input class='eng-in' type='text' value='" + eng.name + "'/><br/>"
-							+ "Url: <input class='eng-in' type='text' value='" + eng.url + "'/>"
-							+ "<br/><input value='save' type='button' /><input value='Default' type='reset'/>"
-							+ "</form>"
-							+ "</div></div>");
-	$('div.draggable-item-list .btn-edit').click(function () {		
-	$(this).parent().find('div.edit-engs').toggle(300);
-});
-$('div.draggable-item-list .btn-delete').click(function () {		
-	if(confirm("u sure?"))$(this).parent().remove();
-});
-$('div.draggable-item-list .reset').click(function () {
-	$(this).parent().find('form')[0].reset();
-});
+	item.find('.btn-edit').click(function () {		
+		$(this).parent().find('div.edit-engs').toggle(300);
+	});
 
-$( ".draggable-item-list" ).draggable({
-	prestart: function()
-	{
+	item.find('.btn-delete').click(function () {		
+		if(confirm("u sure?"))$(this).parent().remove();
+	});
 
-		//przeciąganie ma zamykać tylko element przeciągany
-		$('div.edit-engs').hide();
-	},
-  connectToSortable: ".droppable-list",
-  helper: "clone",
-  revert: "false",
-  revertDuration: 0
-  
-});
+	///????
+	item.find('.reset').click(function () {
+		$(this).parent().find('form')[0].reset();
+	});
 
+	item.draggable({
+		prestart: function()
+		{
+
+			//przeciąganie ma zamykać tylko element przeciągany
+			$('div.edit-engs').hide();
+		},
+		connectToSortable: ".droppable-list",
+		helper: "clone",
+		revert: "false",
+		revertDuration: 0
+	
+	});
 }
+
+
+
 // Przerobić dodawanie grup i silników na jedną uniwersalną funkcję!!!
 
 for (var i = 0; i < background.searchEngines.length ; i++)
@@ -89,32 +72,6 @@ for (var i = 0; i < background.searchGroups.length ; i++)
 							+ "'>" + background.searchGroups[i].name
 							+ "</div>");
 }
-
-	
-/*****************************************************************************/
-
-
-function showPage(target)
-{
-	if(target == undefined || target == null) return;
-	var pages = document.getElementsByClassName("page");
-	for(var i=0; i < pages.length ; i++) {
-		pages[i].style.display = "none";
-	}
-	var tget = document.getElementById(target)
-	if(tget) tget.style.display = "block";
-}
-
-(function () {
-	var menuItems = document.getElementsByClassName("menuItem");
-	for(var i=0; i < menuItems.length ; i++) {
-		menuItems[i].addEventListener("click", function () {
-			showPage(this.getAttribute("data-target"));		
-		})
-	}
-})();
-
-//////////////////////////////////
 
 $(".droppable-list").sortable({
   revert: 150,
